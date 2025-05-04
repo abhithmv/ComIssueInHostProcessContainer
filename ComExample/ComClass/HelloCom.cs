80e86df427588cd08265d237779e7d99ac40656d
abhithmv <71448419+abhithmv@users.noreply.github.com>
2025-05-05 01:11:35 +0530

    Add files via upload

---
+++ ComExample/ComClass/HelloCom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ComClass
+{
+    // Define an interface for COM
+    [ComVisible(true), Guid("B2C3D4E5-F6A1-2345-6789-0ABCDE123456")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IHelloCom
+    {
+        string SayHello(string name);
+    }
+
+
+    // Make the class COM-visible
+    [ComVisible(true), Guid("A1B2C3D4-E5F6-1234-5678-90ABCDEF1234")]
+    internal class HelloCom : IHelloCom
+    {
+        public string SayHello(string name)
+        {
+            return $"Hello, {name} from COM!";
+        }
+    }
+}
---
+++ ComExample/ComExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using ComClass;
+using System;
+
+namespace ComExample
+{
+    class Program
+    {
+        private const string ComInterfaceGuid = "A1B2C3D4-E5F6-1234-5678-90ABCDEF1234";
+
+        static void Main(string[] args)
+        {
+            var type = Type.GetTypeFromCLSID(Guid.Parse(ComInterfaceGuid));
+            // Create an instance of the COM object 
+            var comObject = Activator.CreateInstance(type) as IHelloCom;
+            Console.WriteLine(comObject.SayHello("World"));
+        }
+    }
+}
